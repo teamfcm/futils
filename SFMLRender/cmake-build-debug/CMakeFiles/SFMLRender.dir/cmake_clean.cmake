404d2002126199900381e7b4dc145237f3ae01ac
FelixGanz <felix.ganz@epitech.eu>
2017-07-21 19:47:45 +0200

    New CLion Project SFMLRender

---
+++ SFMLRender/cmake-build-debug/CMakeFiles/SFMLRender.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SFMLRender.dir/library.cpp.o"
+  "libSFMLRender.pdb"
+  "libSFMLRender.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SFMLRender.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ SFMLRender/library.h
@@ -0,0 +1,6 @@
+#ifndef SFMLRENDER_LIBRARY_H
+#define SFMLRENDER_LIBRARY_H
+
+void hello();
+
+#endif
