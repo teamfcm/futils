2560b25ec67081668c1907b4f54ed8399772726b
Arroganz <felix.ganz@epitech.eu>
2018-01-07 15:34:51 +0100

    windows modifying cmake for cross platform

--- utils/log.hpp
+++ utils/log.hpp
@@ -88,7 +88,7 @@ namespace flog
         template    <typename T>
         void        err(T const &obj, std::string const &func = DEFAULT_FUNC_NAME)
         {
-            std::scoped_lock    lock(this->mutex);
+            std::scoped_lock<std::mutex>    lock(this->mutex);
 
             if (this->previousFunc != func)
             {
